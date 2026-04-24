415d220d12d2b12d05b318f7204c7421c3b92cc3
ojoro <zaiya4002@gmail.com>
2026-04-23 20:49:48 -0700

    set up basic rest endpoints on flask server, created login & register html pages

---
+++ web/app.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, jsonify, request
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return jsonify({"hello":"This is main page"})
+
+
+@app.route('/register', methods = ['GET','POST'])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    
+    username = request.form["username"]
+    passowrd = request.form["password"]
+
+
+
+    #MAKE CALL TO DOCKER CONTAIN DB HERE TO ADD USER TO DB
+
+    return ""
+
+@app.route('/login', methods=['GET, POST'])
+def login():
+    if request.method == 'GET':
+        return render_template("login.html")
+
+    username = request.form["username"]
+    password = request.form["password"]
+
+
+
+
+    #****make a call to docker container storing db to verify/authetnicate user
+    
+    
+    
+    
+    
+    
+    
+    return ""
+
+
+
+
+#get endpoint for looking for users
+@app.route('/search', methods=['GET'])
+def search():
+    pass
+
+
+
+#post end point for searching for users
+@app.route('/lookup/<username>', methods='POST')
+def lookup(username):
+    pass
+
+
+
+
+
+
